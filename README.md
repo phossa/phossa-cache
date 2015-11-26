374f5e938eef611117b5f4f109233e685a159d37
Hong Zhang <phossa@126.com>
2015-11-26 10:24:03 +0800

    init
    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Introduction
+Phossa is a lightweight PHP framework. Phossa-cache is caching package.
+
+# Features
+
+- Support PHP 5.4+
+
+- PHP7 ready for return type declarations and argument type declarations.
+
+# Version
+1.0.0
+
+# Dependencies
+PHP >= 5.4.0 and Phossa-shared 1.*
---
+++ src/Phossa/Cache/Exception/ExceptionInterface.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Phossa Project
+ *
+ * @see         http://www.phossa.com/
+ * @copyright   Copyright (c) 2015 phossa.com
+ * @license     http://mit-license.org/ MIT License
+ */
+/*# declare(strict_types=1); */
+
+namespace Phossa\Cache\Exception;
+
+/**
+ * Exception marker for phossa-cache
+ *
+ * @interface
+ * @package \Phossa\Cache
+ * @author  Hong Zhang <phossa@126.com>
+ * @version 1.0.0
+ * @since   1.0.0 added
+ */
+interface ExceptionInterface extends \Phossa\Shared\Exception\ExceptionInterface
+{
+
+}
